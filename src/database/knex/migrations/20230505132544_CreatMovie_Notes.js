ec8782c13622b27bd5edd3805a878e87cd258f49
ygor <ygor.rochaferreira@gmail.com>
2023-05-05 11:19:24 -0300

    created tags

--- src/database/knex/migrations/20230505132544_CreatMovie_Notes.js
+++ src/database/knex/migrations/20230505132544_CreatMovie_Notes.js
@@ -5,7 +5,7 @@ exports.up = knex => knex.schema.createTable("Movie_Notes", table => {
     table.text("title");
     table.text("description");
     table.integer("score of the movie").checkIn([1, 2, 3, 4, 5]); // restrição de check
-    table.integer("users_id").references("id").inTable("users");
+    table.integer("user_id").references("id").inTable("users");
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
 });
