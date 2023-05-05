f3cacc00b1e777ab66e13564b57513ddeebec448
ygor <ygor.rochaferreira@gmail.com>
2023-05-05 11:09:32 -0300

    Knex config

---
+++ src/database/knex/index.js
@@ -0,0 +1,7 @@
+const config = require("../../../knexfile");
+
+const knex = require("knex");
+
+const connection = knex.connect(config,development);
+
+module.exports = connection;
---
+++ src/database/knex/migrations/20230505132544_CreatMovie_Notes.js
@@ -0,0 +1,15 @@
+
+exports.up = knex => knex.schema.createTable("Movie_Notes", table => {
+    
+    table.increments("id");
+    table.text("title");
+    table.text("description");
+    table.integer("score of the movie").checkIn([1, 2, 3, 4, 5]); // restrição de check
+    table.integer("users_id").references("id").inTable("users");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+});
+
+
+exports.down = knex => knex.schema.dropTable("Movie_Notes") 
+
