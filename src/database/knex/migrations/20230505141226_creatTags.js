3336c96776942319b006857ee2ed24b7ad5a28cb
ygor <ygor.rochaferreira@gmail.com>
2023-05-05 18:59:18 -0300

    showing and listing notes

--- src/database/knex/migrations/20230505141226_creatTags.js
+++ src/database/knex/migrations/20230505141226_creatTags.js
@@ -2,7 +2,7 @@ exports.up = knex => knex.schema.createTable("tags", table => {
     
     table.increments("id");
     table.text("name").notNullable();
-    table.integer("note").references("id").inTable("Movie_Notes").onDelete("CASCADE");;
+    table.integer("note_id").references("id").inTable("Movie_Notes").onDelete("CASCADE");;
     table.integer("user_id").references("id").inTable("users");
 
     
